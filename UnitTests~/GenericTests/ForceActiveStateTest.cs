18081c6cd1810994fc9f82d0460e5c553f2adbf2
dag-ed <gw_dagda@mac.com>
2026-07-06 12:24:10 +0900

    Test Force Active State pass execution

--- UnitTests~/GenericTests/ForceActiveStateTest.cs
+++ UnitTests~/GenericTests/ForceActiveStateTest.cs
@@ -1,11 +1,19 @@
 using nadena.dev.modular_avatar.core;
 using nadena.dev.modular_avatar.core.editor;
+using nadena.dev.ndmf.platform;
 using NUnit.Framework;
+using BuildContext = nadena.dev.ndmf.BuildContext;
 
 namespace modular_avatar_tests
 {
     public class ForceActiveStateTest : TestBase
     {
+        private class DummyPlatformProvider : INDMFPlatformProvider
+        {
+            public string QualifiedName => "TestPlatform";
+            public string DisplayName => QualifiedName;
+        }
+
         [Test]
         public void ForcesActiveObjectInactive()
         {
@@ -51,6 +59,20 @@ namespace modular_avatar_tests
             Assert.AreEqual(2, appliedCount);
             Assert.IsFalse(first.activeSelf);
             Assert.IsTrue(second.activeSelf);
+        }
+
+        [Test]
+        public void BuildContextPassForcesState()
+        {
+            var root = CreateRoot("root");
+            var child = CreateChild(root, "child");
+            child.SetActive(false);
+            child.AddComponent<ModularAvatarForceActiveState>().Active = true;
+            var context = new BuildContext(root, null, new DummyPlatformProvider());
+
+            new ForceActiveStatePass().Process(context);
+
+            Assert.IsTrue(child.activeSelf);
         }
     }
 }
